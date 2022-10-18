cda130a822cb26e1dff0ea035eb47918a243647b
Martin Bulán <martin.bulan@akka.eu>
2022-10-18 21:00:06 +0200

    Update Blockchain class (create block method)

--- src/blockchain.py
+++ src/blockchain.py
@@ -1,5 +1,24 @@
+import datetime
+import json
+import hashlib
+
 class Blockchain:
 
     def __init__(self):
+
+        # Initialize blockchain
         self.chain = []
-        self.create_block(proof = 1, prev_hash = '0')
+
+        #Create first block
+        self.create_block(nonce = 1, prev_hash = '0')
+
+    # Create new block an append it to the chain
+    def create_block(self, nonce, prev_hash):
+        block = {'index': len(self.chain)+1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'nonce': nonce,
+                 'prev_hash': prev_hash
+                }
+
+        self.chain.append(block)
+        return block
