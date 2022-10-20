63851ac3d04fda7e14072ad4dd3e3be106314506
Martin Bulán <martin.bulan@akka.eu>
2022-10-20 21:47:25 +0200

    Update blockchain.py, server.py

--- src/blockchain.py
+++ src/blockchain.py
@@ -1,24 +1,66 @@
 import datetime
-import json
 import hashlib
+import json
 
 class Blockchain:
 
     def __init__(self):
-
-        # Initialize blockchain
         self.chain = []
-
-        #Create first block
+        self.leading_zeros = 4
         self.create_block(nonce = 1, prev_hash = '0')
-
-    # Create new block an append it to the chain
+        
     def create_block(self, nonce, prev_hash):
         block = {'index': len(self.chain)+1,
                  'timestamp': str(datetime.datetime.now()),
                  'nonce': nonce,
-                 'prev_hash': prev_hash
+                 'prev_hash': prev_hash,
+                 'current_complexity': self.leading_zeros
                 }
 
         self.chain.append(block)
         return block
+
+    def get_prev_block(self):
+        return self.chain[-1]
+
+    def proof_of_work(self, prev_block):
+        nonce = 1
+        check_nonce = False
+        while check_nonce == False:
+            hash_operation = hashlib.sha256(json.dumps(str(prev_block) + str(nonce), sort_keys=True).encode()).hexdigest()
+
+            if hash_operation[:self.leading_zeros] == self.leading_zeros*'0':
+                check_nonce = True
+            else:
+                nonce += 1
+
+        return nonce
+
+    def hash(self, prev_block, nonce):
+        
+        return hashlib.sha256(json.dumps(str(prev_block) + str(nonce), sort_keys=True).encode()).hexdigest()
+
+
+    def validate(self, chain):
+
+        prev_block = chain[0]
+        block_index = 1
+
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block['prev_hash'] != self.hash(prev_block, block['nonce']):
+
+                return False
+
+            nonce = block['nonce']
+            hash_operation = hashlib.sha256(json.dumps(str(prev_block) + str(nonce), sort_keys=True).encode()).hexdigest()
+
+            if hash_operation[:block['current_complexity']] != block['current_complexity']*'0':
+
+                return False
+
+            prev_block = block
+            block_index += 1
+
+        return True
+
--- src/server.py
+++ src/server.py
@@ -4,3 +4,52 @@ from blockchain import Blockchain
 app = Flask(__name__)
 
 blockchain = Blockchain()
+
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    prev_block = blockchain.get_prev_block()
+
+    nonce = blockchain.proof_of_work(prev_block)
+    prev_hash = blockchain.hash(prev_block, nonce)
+    block = blockchain.create_block(nonce, prev_hash)
+
+    response = {'Blockchain': 'New block was mined!',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'nonce': block['nonce'],
+                'previous_hash': block['prev_hash'],
+                'current_complexity': block['current_complexity']}
+
+    return jsonify(response), 200
+
+@app.route('/get_chain', methods=['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)}
+
+    return jsonify(response), 200
+
+
+@app.route('/complexity/', methods=['GET'])
+@app.route('/complexity/<int:leading_zeros>', methods=['GET'])
+def set_complexity(leading_zeros = None):
+
+    if leading_zeros:
+        blockchain.leading_zeros=leading_zeros
+
+        response = {'Leading zeros set to ': blockchain.leading_zeros}
+        return jsonify(response), 200
+
+    else:
+
+        response = {'Current number of leading zeros ': blockchain.leading_zeros}
+        return jsonify(response), 200
+
+@app.route('/validate', methods=['GET'])
+def validate():
+
+    response = {'Chain valid': blockchain.validate(blockchain.chain)}
+
+    return jsonify(response), 200
+
+app.run(host = '0.0.0.0', port = 5000)
